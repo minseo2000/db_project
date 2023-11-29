c75438f2306882a5d8d0aee909b282e5ecb99145
minseo2000 <qlqlxks123@naver.com>
2023-11-29 20:41:38 +0900

    update

--- codes/frontend/netflix/src/App.js
+++ codes/frontend/netflix/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
- import LoginPage from './pages/LoginPage.jsx';
-// import SignUpPage from './pages/SignUpPage.jsx';
+// import LoginPage from './pages/LoginPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
 //import HomePage from './pages/HomePage.jsx';
 
 function App() {
   return (
     <>
       {/* <LoginPage /> */}
-      {/* <SignUpPage /> */}
-      <LoginPage />
+      <SignUpPage />
+
     </>
   );
 }
