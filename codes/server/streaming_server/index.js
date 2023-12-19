04cb063646f6288ca100a0b87cfc68875f82256d
minseo2000 <qlqlxks123@naver.com>
2023-12-19 14:47:18 +0900

    update

--- codes/server/streaming_server/index.js
+++ codes/server/streaming_server/index.js
@@ -6,6 +6,23 @@ const port = 50011;
 
 app.use(express.static("public"));
 
+app.get("/image/:fileName", (req, res) => {
+    const fileName = req.params.fileName;
+    const filePath = `./images/${fileName}`; // 확장자가 이미지 파일 이름에 포함되어 있다고 가정
+
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.error(`File access error for ${filePath}.`);
+            res.sendStatus(404); // 파일이 없으면 404 Not Found
+            return;
+        }
+
+        res.setHeader("Content-Type", "image/png"); // 적절한 MIME 타입 설정
+        const fileStream = fs.createReadStream(filePath);
+        fileStream.pipe(res);
+    });
+});
+
 // 파일 경로를 '/video/:fileName' 형식으로 받음
 app.get("/video/:fileName", (req, res) => {
     const fileName = req.params.fileName;
